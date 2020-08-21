beb1889dd4d4d01d26328f270973f20be91f9de0
Igor Polonskiy <barlogfire@ya.ru>
2020-08-21 15:21:50 +0300

    карусель

---
+++ script.js
@@ -0,0 +1,49 @@
+let slider = document.querySelector('.slider');
+let dots = document.querySelectorAll('.dot')
+// create a simple instance
+// by default, it only adds horizontal recognizers
+let mc = new Hammer(slider);
+let translate = 0
+// listen to events...
+mc.on("swipeleft swiperight", function (ev) {
+    let whidth = slider.clientWidth
+    if (ev.type == 'swipeleft') {
+        translate -= whidth;
+    } else translate += whidth;
+
+    if (translate < -3 * whidth) {
+        translate = 0
+    } else if (translate > 0) {
+        translate = -3 * whidth
+    }
+    slider.style.transform = `translateX(${translate}px)`;
+    switch (translate) {
+        case -3 * whidth:
+            dots.forEach((dot) => {
+                dot.classList.remove('active');
+            })
+            dots[3].classList.add('active');
+            break
+
+        case -2 * whidth:  // if (x === 'value2')
+            dots.forEach((dot) => {
+                dot.classList.remove('active');
+            })
+            dots[2].classList.add('active');
+            break
+
+        case -1 * whidth:  // if (x === 'value2')
+            dots.forEach((dot) => {
+                dot.classList.remove('active');
+            })
+            dots[1].classList.add('active');
+            break
+
+        default:
+            dots.forEach((dot) => {
+                dot.classList.remove('active');
+            })
+            dots[0].classList.add('active');
+            break
+    }
+});
